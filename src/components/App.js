f6147871a5b0fd7aba3f559a6e30f8d696f2d70e
zinvergo93 <zachary.coding@gmail.com>
2020-10-19 17:59:27 -0500

    initial commit. Route/Switch

---
+++ src/components/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Navigation from './navigation';
+import PageContent from './page-content';
+
+
+
+function App() {
+  return (
+    <div className="App">
+     <h1>Zac Invergo writes some code</h1>
+     <Navigation />
+     <PageContent />
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/workflow.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+export default class Workflow extends Component {
+    render() { 
+        return (  <h1> Workflow...</h1>);
+    }
+}
+ 
+ 
